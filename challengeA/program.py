5c17af860125bcb54c9159a11db87ca3a95abb6d
Verite Mugabo <mugaboverite@gmail.com>
2025-07-20 22:29:45 -0400

    ChallengeA: improved

--- challengeA/program.py
+++ challengeA/program.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-
 import sys
 
+
 def count_buildings(building: list[int]) -> int:
-    total_buildings = 0
+    total_buildings = 1
+    curr = building[0]
 
-    for index in range(len(building) - 1):
-        if building[index] > building[index+1]:
+    for el in building[1:]:
+        if curr >= el:
             total_buildings += 1
+        curr = el
 
-    total_buildings += 1
     return total_buildings
 
 
@@ -17,6 +18,7 @@ def main():
     for line in sys.stdin:
         build = [int(x) for x in line.split()]
         print(count_buildings(build))
+
 
 if __name__ == "__main__":
     main()
