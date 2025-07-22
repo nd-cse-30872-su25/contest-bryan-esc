f3f1c71700515d0210a499fc06016f092789034d
Verite Mugabo <mugaboverite@gmail.com>
2025-07-21 23:38:47 -0400

    ChallengeE: done

--- challengeE/program.py
+++ challengeE/program.py
@@ -6,22 +6,24 @@ import sys
 def find_paths(
     tree: list[int], target: int, total: int, path: list[int], index: int = 0
 ):
-    if target == total:
-        yield path
-
     left = 2 * index + 1
     right = 2 * index + 2
+    leaf = (left >= len(tree) or tree[left] == 0) and (
+        right >= len(tree) or tree[right] == 0
+    )
+    if target == total and leaf:
+        yield path
 
     if left < len(tree) and tree[left] != 0:
         if total + tree[left] <= target:
             yield from find_paths(
-                tree, target, total+tree[left], path + [tree[left]], left
+                tree, target, total + tree[left], path + [tree[left]], left
             )
 
     if right < len(tree) and tree[right] != 0:
         if total + tree[right] <= target:
             yield from find_paths(
-                tree, target, total+tree[right], path + [tree[right]], right
+                tree, target, total + tree[right], path + [tree[right]], right
             )
 
 
@@ -29,8 +31,25 @@ def main():
     for line in sys.stdin:
         target = int(line)
         tree = list(map(int, sys.stdin.readline().split()))
-        for path in find_paths(tree, target, tree[0], [tree[0]]):
-            print(f"{target}:", ", ".join(list(map(str, path))))
+        res = list(find_paths(tree, target, tree[0], [tree[0]]))
+        res = sorted(res)
+        if not res:
+            print(f"{target}:")
+        else:
+            for path in res:
+                print(f"{target}:", ", ".join(list(map(str, path))))
+
+
+# def main():
+#     for line in sys.stdin:
+#         target = int(line)
+#         tree = list(map(int, sys.stdin.readline().split()))
+#         res = list(find_paths(tree, target, tree[0], [tree[0]]))
+#         result = list(enumerate(res))
+#         sorted_result = sorted(result, key=lambda x: x[1])
+#         result_2 = [row for index, row in sorted_result]
+#         for path in result_2:
+#             print(f"{target}:", ", ".join(list(map(str, path))))
 
 
 if __name__ == "__main__":
