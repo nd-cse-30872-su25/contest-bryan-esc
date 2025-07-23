385a08c92b376e5187e37a628e2b67ed4d27c206
Verite Mugabo <mugaboverite@gmail.com>
2025-07-23 00:47:11 -0400

    done

--- challengeG/program.py
+++ challengeG/program.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
-from collections import defaultdict
-
-Graph = dict[int, list[int]]
 
 
-def find_circuit(graph: Graph, start: int, vertex: int, visited: set[int], path:
-                 list[tuple[int,int]], vertices: int)-> list[tuple[int, int]]:
-    if path and start == vertex and len(path) == vertices:
-        return path
-
-    for neighbor in graph[vertex]:
-
-        if neighbor in visited:
-            continue
-        
-        if neighbor == start and len(path) < vertices - 1:
-            continue
-
-        visited.add(graph[vertex][neighbor])
-
-        path.append((vertex, neighbor))
-
-        if find_circuit(graph, start, neighbor, visited, path, vertices):
-            return path
-
-        path.pop(-1)
-
-        visited.remove(graph[vertex][neighbor])
-    return []
-
-def find_paths(graph: Graph, nvertces: int) -> list[tuple[int, int]]:
-    start = sorted(graph.keys())[0]
+def solve(grid: list[list[int]], vertices: int) -> int:
     visited = set()
-    return find_circuit(graph, start, start, visited, [], nvertces)
+
+    isolated_nodes = 0
+    for vertex in range(vertices):
+        if vertex not in visited:
+            frontier = [vertex]
+            visited.add(vertex)
+            while frontier:
+                node = frontier.pop()
+                for neighbor in range(vertices):
+                    if grid[node][neighbor] and neighbor not in visited:
+                        visited.add(neighbor)
+                        frontier.append(neighbor)
+            isolated_nodes += 1
+    return isolated_nodes
+
 
 def main():
+    count = 1
     while True:
         try:
             n = int(sys.stdin.readline())
@@ -45,13 +31,8 @@ def main():
         grid = []
         for _ in range(n):
             grid.append(list(map(int, sys.stdin.readline().split())))
-
-        graph: Graph = defaultdict(list)
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] != 0:
-                    graph[i].append(j)
-        print(find_paths(graph, n))
+        print(f"System {count} isolated circuits: {solve(grid, n)}")
+        count += 1
 
 
 if __name__ == "__main__":
